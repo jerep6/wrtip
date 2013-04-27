4c8afae3470e24c7a0168779cea8b56431d010f3
jerep6 <jerep6@gmail.com>
2013-04-27 12:28:28 +0200

    switch to git (v1.3)

---
+++ data/a.js
@@ -0,0 +1,12 @@
+/** Get selection en transmit it to add-on.
+ * When a "context" listener returns a string, it becomes the item's new label. At this time 2013-03-06 content script doesn't handle
+ * localisation. So I use postMessage
+ */
+self.on("context", function(node) {
+    self.postMessage({"event" : "context", "payload" : window.getSelection().toString()});
+    return true;
+});
+
+self.on("click", function(node, data) {
+    self.postMessage({"event" : "click", "payload" : window.getSelection().toString()});
+});
---
+++ data/js/widget.js
@@ -0,0 +1,3 @@
+self.port.on("init", function(payload) {
+  document.getElementById("source").innerHTML = "payload";
+}); 
