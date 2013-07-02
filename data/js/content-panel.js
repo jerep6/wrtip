c1916615a9a12ceb05a2f4f2af6d6ec389b50c3b
Jérémy Pinsolle <jerep6@gmail.com>
2013-07-02 22:25:53 +0200

    Sanitize html code receive from WR to avoid XSS

--- data/js/content-panel.js
+++ data/js/content-panel.js
@@ -8,17 +8,16 @@ self.port.on("newTranslation", function(obj) {
   var parser = new DOMParser();
   var wrPage = parser.parseFromString(obj.html, "text/html");
   
+  
   // Inject it into html balise of the panel
   $("html").html(wrPage.getElementsByTagName('html')[0].innerHTML);
-  
-  
+
   // Get search form
   var form = $("form[name='sbox']");
   if(form != null && form != undefined) {
     // Write into input field the search word and select it with focus. Thanks to that, scrollbar returns to the top
     var inputSearch =  form.find("input[name='w']");
     inputSearch.val(obj.search).focus().select();
-    inputSearch.removeAttr("onKeyDown");
     
     // Override the submit method. I don't want to reload page because it erase content script
     form.submit(function() {
@@ -33,6 +32,7 @@ self.port.on("newTranslation", function(obj) {
   
 });
 
+
 self.port.on("hide", function(panelWidth) {
   // Compute left according to panel size
   var left = (panelWidth - 180) / 2;
@@ -43,6 +43,9 @@ self.port.on("hide", function(panelWidth) {
   // Append to DOM
   $("html").prepend(imgLoading);
 });
+
+
+
 
 
 // I can't use iframe due to same origin policy 
