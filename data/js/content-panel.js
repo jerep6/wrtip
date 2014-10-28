e810b6d3187c93ee9a3bdbe46e6383f004c261be
jerep6 <jerep6@gmail.com>
2014-10-28 16:53:29 +0100

    use mobile version + config

--- data/js/content-panel.js
+++ data/js/content-panel.js
@@ -3,7 +3,7 @@ var imgLoading = $('<img />').attr("src", "../img/loading.gif");
 imgLoading.css({"position":"absolute", "top":"100px", "left": "0px", "width":"180px", "opacity":"0.1"});
 
 
-self.port.on("newTranslation", function(obj) {
+self.port.on("newTranslationAPI", function(obj) {
   // Create a dom element with the result page from WordReference
   var parser = new DOMParser();
   var wrPage = parser.parseFromString(obj.html, "text/html");
@@ -11,8 +11,63 @@ self.port.on("newTranslation", function(obj) {
   
   // Inject it into html balise of the panel
   $("html").html(wrPage.getElementsByTagName('html')[0].innerHTML);
+  customizeForm(self, obj);
+});
 
-  // Get search form
+
+self.port.on("newTranslationMOBILE", function(obj) {
+  // Create a dom element with the result page from WordReference
+  var parser = new DOMParser();
+  var wrPage = parser.parseFromString(obj.html, "text/html");
+  var html = wrPage.getElementsByTagName('html')[0].innerHTML;
+  html = html.replace("width = 450", "width = 300");
+  html = html.replace(/&nbsp;/g, "&nbsp; ");
+  
+  // Inject it into html balise of the panel
+  document.getElementsByTagName('html')[0].innerHTML = html;
+  //$("html").html(wrPage.getElementsByTagName('html')[0].innerHTML);
+  
+  // Override some properties
+  $("head").append('<link rel="stylesheet" href="http://mini.wordreference.com/2012/scripts/styleext.css" type="text/css" media="screen" />');
+  $(".WRreporterror").remove();
+  $("#threadsHeader").remove();
+  $("#lista_link").remove();
+  $("#extra_links").remove();
+  $(".layout > tbody > tr:last").remove();
+
+  $("#Otbl").css("width", "100%");
+  $("td.content").css("width", "100%");
+  $("table.WRD").css("width", "100%");
+  $("#logo a").css("font-size", "17px");
+  $("#header").css("height", "20px");
+  $("#header").css("line-height", "20px");
+  $("#logo").css("line-height", "20px");
+  $("td.namecell").css("height", "20px");
+  
+  $("td").css("padding", "0");
+  
+  customizeForm(self, obj);
+
+});
+
+self.port.on("hide", function(panelWidth) {
+  // Compute left according to panel size
+  var left = (panelWidth - 180) / 2;
+  
+  // Override left 
+  imgLoading.css("left", left+"px");
+  
+  // Append to DOM
+  $("html").prepend(imgLoading);
+});
+
+/**
+ * Add custom behavior to panel
+ * Handle form submit
+ * Provide open in new tab link
+ */
+function customizeForm(self, obj) {
+// Get search form
   var form = $("form[name='sbox']");
   if(form != null && form != undefined) {
     // Write into input field the search word and select it with focus. Thanks to that, scrollbar returns to the top
@@ -47,23 +102,8 @@ self.port.on("newTranslation", function(obj) {
 		self.port.emit("openTab", {"search": wordToTranslate, "language": language});  
 		return false;
 	});
-  }
-});
-
-
-self.port.on("hide", function(panelWidth) {
-  // Compute left according to panel size
-  var left = (panelWidth - 180) / 2;
-  
-  // Override left 
-  imgLoading.css("left", left+"px");
-  
-  // Append to DOM
-  $("html").prepend(imgLoading);
-});
-
-
-
+  }	
+}
 
 
 // I can't use iframe due to same origin policy 
